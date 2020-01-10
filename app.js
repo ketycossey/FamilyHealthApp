43d900f9d6161cbb70ceccd9e56d9aa3b2d669b7
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-01-10 09:13:22 -0600

    my commit

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
+const careprovidersRouter = require('./routes/careproviders')
+const familiesRouter = require('./routes/families')
+const membersRouter = require('./routes/members')
+const medicationRouter = require('./routes/medications')
 const PORT = process.env.PORT || 8080
 // const PORT = 3000
 require('dotenv').config()
@@ -17,10 +21,7 @@ app.engine('mustache', mustacheExpress(VIEWS_PATH + './partials', '.mustache'))
 app.set('views',VIEWS_PATH)
 app.set('view engine', 'mustache')
 
-const careprovidersRouter = require('./routes/careproviders')
-const familiesRouter = require('./routes/families')
-const membersRouter = require('./routes/members')
-const medicationRouter = require('./routes/medications')
+
 // const indexRouter = require('./routes/index')
 
 
@@ -29,7 +30,10 @@ app.use('/careproviders', careprovidersRouter)
 app.use('/families', familiesRouter)
 app.use('/members', membersRouter)
 app.use('/medications', medicationRouter)
-
+app.get('/', (req, res) => {
+    console.log("just for testing if it is running or not")
+    res.send("I AM RUNNING if you are testing me:)")
+})
 app.listen(PORT, ()=>{
     console.log("Server is running...")
 })
