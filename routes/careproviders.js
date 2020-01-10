aebdb333204951658384e8330dbb08d983a96b32
Ian Sear <iansear@Ians-MacBook-Pro.local>
2020-01-10 14:03:08 -0600

    adding care provider

--- routes/careproviders.js
+++ routes/careproviders.js
@@ -5,4 +5,19 @@ router.get("/", (req, res) => {
     models.CareProviders.findAll().then(providers => res.render("careproviders", {providers: providers}))
 })
 
+router.post("/", (req, res) => {
+    const provider = models.CareProviders.build({
+        doctor_name: req.body.doctor_name,
+        address: req.body.address,
+        phone: req.body.phone,
+        email: req.body.email,
+        website: req.body.website,
+        specialty: req.body.specialty,
+        member_id: req.body.member_id
+    })
+    provider.save().then(() => {
+        res.redirect("/careproviders")
+    })
+})
+
 module.exports = router
