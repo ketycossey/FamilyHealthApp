0a06344ac7147f1934d57d28fc0fe607898e5569
Alejandra Guzman <aleguzman.gza@gmail.com>
2020-01-10 09:47:29 -0600

    Register post with hash password

--- routes/register.js
+++ routes/register.js
@@ -4,5 +4,19 @@ const router = express.Router()
 app.get('/',(req,res) => {
     res.render('register')
 })
+app.post('/register',(req,res) => {
+
+    const familyName = req.body.family_name 
+    const username = req.body.username
+    const password = req.body.password
+    const address = req.body.address
+
+    bcrypt.hash(password, SALT_ROUNDS).then(hash => {
+        db.none('INSERT INTO users(familyName, username, password, address ) VALUES($1,$2,$3,$4)',[familyName, username, hash, address]).then(() => {
+            res.redirect('/login')
+        })
+    })
+
+})
 
 module.exports = router
