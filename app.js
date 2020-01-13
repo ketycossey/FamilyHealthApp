6c822fd88128fefa596e5bf2fa44cd7b5c35354b
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-01-13 09:19:10 -0600

    upload image file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const medicationRouter = require('./routes/medications')
 const indexRouter = require('./routes/index')
 const registerRouter = require('./routes/register')
 const loginRouter = require('./routes/login')
-const labresultsRouter = require('./routes/labresults')
+const labresultsRouter = require('./routes/testlabs')
 const PORT = process.env.PORT || 8080
 require('dotenv').config()
 const path = require('path')
@@ -19,14 +19,14 @@ const VIEWS_PATH = path.join(__dirname,'/views')
 const session = require("express-session")
 app.use(session({
     secret: "He who has a why to live can bear almost any how",
-    resave: false,
-    saveUninitialized: true
+    resave: true, //true?
+    saveUninitialized: false //false? 
 }))
 
-//global.models = require("./models")
-
-const models = require('./models')
-models.family.findAll().then(r => console.log(r))
+global.models = require("./models")
+global.__basedir = __dirname 
+//const models = require('./models')
+//models.family.findAll().then(r => console.log(r))
 
 
 function auth(req,res,next) {
@@ -37,7 +37,12 @@ function auth(req,res,next) {
     }
 }
 
-app.use(express.static(__dirname + '/public'))
+app.use('/uploads', express.static('uploads'))
+app.use('/css', express.static('css'))
+// changed the public static folder
+app.use(express.static('public'))
+//app.use('/public', express.static('public'))
+//app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
 app.set('views',VIEWS_PATH)
