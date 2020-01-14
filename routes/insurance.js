d84973ed8a3cd6fe5d93ad1591d3e31c9590722e
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-01-14 16:51:26 -0600

    insurance

--- routes/insurance.js
+++ routes/insurance.js
@@ -6,16 +6,16 @@ const models = require('../models')
 const fs = require('fs')
 
 router.get('/', async (req, res) => {
-  let member_id = req.params.member_id
-  let result = await models.Insurance.findAll({
+  
+  let insurance = await models.Insurance.findAll({
     where: {
-      member_id: member_id
+      member_id: req.session.member_id
     }
   })
-  if(result != null) {
-    res.render('insurance')
+  if(insurance != null) {
+    res.render('insurance', {insurance: insurance})
   } else {
-    res.render('insurance', {message: "No insurance uploaded"})
+    res.render('insurance', {message: "Upload insurance to display"})
   }
   
 })
