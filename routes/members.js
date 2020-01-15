663c633df644b3b9e8bf900e3a75445f0acc1dc3
Baylie <bayliewarrick2019@gmail.com>
2020-01-15 10:32:03 -0600

    added some things

--- routes/members.js
+++ routes/members.js
@@ -26,7 +26,7 @@ router.post('/add/',(req,res) => {
         family_id: req.session.family.userId
     })
     member.save().then(savedMember => console.log(savedMember))
-    
+    res.redirect("/members")
 })
 
 router.get('/update/:member',(req,res) => {
@@ -43,6 +43,7 @@ router.get('/add',(req,res) => {
   
     res.render('add', {id: req.session.family.userId})
 
+
 })
 
 //<Localhost>:<port>/members/update/<memberId>
@@ -57,8 +58,9 @@ router.post('/update/:memberId',(req,res) => {
             where: {
                 id: req.params.memberId,
             }
-    })
-    res.redirect('/members')
+    }).then(
+        res.redirect('/members')
+    )
 })
 
 //<Localhost>:<port>/members/delete/<memberId>
@@ -67,8 +69,9 @@ router.post('/delete/:memberId',(req,res) => {
         where: {
             id: req.params.memberId
         }
-    })
-    res.redirect('members')
+    }).then(
+        res.redirect('/members')
+    )
 })
 
 router.post("/member", async (req, res) => {
