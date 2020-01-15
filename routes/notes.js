4f957516e25d085612f72d5ed7b590556fc19b91
Alejandra Guzman <aleguzman.gza@gmail.com>
2020-01-15 10:31:38 -0600

    update

--- routes/notes.js
+++ routes/notes.js
@@ -3,26 +3,24 @@ const router = express.Router()
 
 
 //add note
-// router.post("/", async (req, res) => {
-//     let title = req.body.noteTitle;
-//     let body = req.body.noteBody;
-//     let notes = models.notes.build({
-//       title: title,
-//       body: body
-//     })
-//       res.render("notes")
-//   });
+router.post("/", async (req, res) => {
+    let title = req.body.title
+    let body = req.body.body
+    await models.notes.build({
+      title: title,
+      body: body
+    })
+    res.render("notes")
+      
+  });
 
   router.get("/", async (req, res) => {
-    // models.notes.findAll().then(notes =>
-    //   res.render("notes", { notes: notes })
-    // )
-    let allnotes = await models.notes.findAll({
+   let notes = await models.notes.findAll({
         where: {
-            family_id: 4
+            family_id: req.session.family.userId
         }
     })
-    res.render('notes', {notes: allnotes})
+    res.render('notes', {notes: notes})
   })
 
 
